6c645fbfdf151a81e54d9369844d1d61a779d64b
nhut1205 <nhuta4@gmail.com>
2024-01-19 21:33:40 +0700

    bai2

---
+++ src/Bai2.java
@@ -0,0 +1,8 @@
+public class Bai2 {
+    public static void main(String[] args) throws Exception {
+        int a = 1;
+        int b = 2;
+        int c = a +b;
+        System.out.println(c);
+    }
+}
